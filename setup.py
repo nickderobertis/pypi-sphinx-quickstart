9e9af40f8f093c36c7822af78c3257ef80844ab1
Nick DeRobertis <nick@claimfound.com>
2019-08-04 17:37:36 -0400

    Conf.py is not being found by setup.py on installation, try adding to package_data

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     author_email=conf.PACKAGE_AUTHOR_EMAIL,
     license=conf.PACKAGE_LICENSE,
     packages=find_packages(),
+    package_data={'': ['conf.py']},
     classifiers=conf.PACKAGE_CLASSIFIERS,
     install_requires=conf.PACKAGE_INSTALL_REQUIRES,
 )
