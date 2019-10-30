08fb8eb684eefe20cf822f6f55a50d1780f0b638
Nick DeRobertis <whoopnip@gmail.com>
2019-10-30 15:53:46 -0400

    Change background color of code blocks and make tables wrap text

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -203,3 +203,4 @@ def skip(app, what, name, obj, would_skip, options):
 def setup(app):
     app.connect("autodoc-skip-member", skip)
     app.add_directive('autosummarynameonly', AutoSummaryNameOnly)
+    app.add_css_file('custom.css')
