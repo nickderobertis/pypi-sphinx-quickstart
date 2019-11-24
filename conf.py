bba11dd847585443a8f82f7c157abbcbed2d79b2
Nick DeRobertis <whoopnip@gmail.com>
2019-11-24 15:39:33 -0500

    Change docsrc and version for testing

--- conf.py
+++ conf.py
@@ -5,7 +5,7 @@
 PACKAGE_NAME = 'py_qs_example'
 
 # Package version in the format (major, minor, release)
-PACKAGE_VERSION_TUPLE = (0, 1, 11)
+PACKAGE_VERSION_TUPLE = (0, 1, 12)
 
 # Short description of the package
 PACKAGE_SHORT_DESCRIPTION = 'Python package PyPi Sphinx quickstart example app'
