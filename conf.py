fca443d95832b2c6d65b7fb6236b616e14eeff71
Nick DeRobertis <whoopnip@gmail.com>
2019-11-30 14:51:07 -0500

    Test adding matplotlib as optional dep

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ REPO_MAINTAINERS = [
 ]
 
 # Package version in the format (major, minor, release)
-PACKAGE_VERSION_TUPLE = (0, 1, 12)
+PACKAGE_VERSION_TUPLE = (0, 1, 13)
 
 # Short description of the package
 PACKAGE_SHORT_DESCRIPTION = 'Python package PyPi Sphinx quickstart example app'
@@ -67,7 +67,7 @@ PACKAGE_INSTALL_REQUIRES = [
 # Keys should be name of the optional feature and values are lists of required packages
 # E.g. {'feature1': ['pandas', 'numpy'], 'feature2': ['matplotlib']}
 OPTIONAL_PACKAGE_INSTALL_REQUIRES = {
-
+    'test': ['matplotlib']
 }
 
 # Sphinx executes all the import statements as it generates the documentation. To avoid having to install all
