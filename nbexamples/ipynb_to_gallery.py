0cbbb5cfc8a5df8c6ba91b488a9cc6b205db335c
Nick DeRobertis <whoopnip@gmail.com>
2019-12-01 16:53:31 -0500

    Recursively convert and copy contents to another folder for ipynb to gallery

--- nbexamples/ipynb_to_gallery.py
+++ nbexamples/ipynb_to_gallery.py
@@ -4,15 +4,19 @@ Dependencies:
 pypandoc: install using `pip install pypandoc`
 """
 import os
+from typing import Optional
 
 import pypandoc as pdoc
 import json
 
 
-def convert_ipynb_to_gallery(file_name):
+def convert_ipynb_to_gallery(file_path: str, out_path: Optional[str] = None):
+    if out_path is None:
+        out_path = file_path.replace('.ipynb', '.py')
+
     python_file = ""
 
-    nb_dict = json.load(open(file_name))
+    nb_dict = json.load(open(file_path))
     cells = nb_dict['cells']
 
     for i, cell in enumerate(cells):
@@ -36,21 +40,44 @@ def convert_ipynb_to_gallery(file_name):
                 python_file = python_file + '\n' * 2 + source
 
     python_file = python_file.replace("\n%", "\n# %")
-    open(file_name.replace('.ipynb', '.py'), 'w').write(python_file)
+    with open(out_path, 'w') as f:
+        f.write(python_file)
 
 
-def convert_all_in_folder_to_gallery(folder: str):
-    files = [file for file in next(os.walk(folder))[2] if file.lower().endswith('ipynb')]
-    for file in files:
-        file_path = os.path.join(folder, file)
-        out_path = file_path.replace('.ipynb', '.py')
-        if os.path.exists(out_path):
-            print(f'Skipping file {file} as .py already exists')
-            continue
-        print(f'Converting file {file}')
-        convert_ipynb_to_gallery(file_path)
+def convert_all_in_folder_to_gallery(folder: str, out_folder: Optional[str] = None, replace: bool = False):
+    folder = os.path.normpath(folder)
+
+    if out_folder is None:
+        out_folder = folder
+    else:
+        out_folder = os.path.normpath(out_folder)
+
+    for path, folders, files in os.walk(folder):
+        sub_path = os.path.sep.join(path.split(os.path.sep)[1:])  # relative path within folder
+        current_out_folder = os.path.join(out_folder, sub_path)
+        print(f'Outputting contents of {sub_path} to {current_out_folder}')
+        if not os.path.exists(current_out_folder):
+            os.makedirs(current_out_folder)
+        files = [file for file in files if file.lower().endswith('ipynb')]
+        for file in files:
+            file_path = os.path.join(path, file)
+            out_path = os.path.join(current_out_folder, file)
+            out_path = out_path.replace('.ipynb', '.py')
+            if not replace and os.path.exists(out_path):
+                print(f'Skipping file {file} as .py already exists')
+                continue
+            print(f'Converting file {file}')
+            convert_ipynb_to_gallery(file_path, out_path)
 
 
 if __name__ == '__main__':
-    import sys
-    convert_all_in_folder_to_gallery(sys.argv[-1])
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('folder',
+                        help='Folder to convert ipynb to Sphinx Gallery py')
+    parser.add_argument('-o', '--out-folder', default=None,
+                        help='Output folder for Sphinx Gallery py files, default in same folder')
+    parser.add_argument('-r', '--replace', action='store_true',
+                        help='Overwrite existing Sphinx Gallery py files')
+    args = parser.parse_args()
+    convert_all_in_folder_to_gallery(args.folder, args.out_folder, args.replace)
