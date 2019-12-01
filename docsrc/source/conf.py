bc6e834d70b84c4b980490796ac42a9ca8fefcb6
Nick DeRobertis <whoopnip@gmail.com>
2019-12-01 18:27:53 -0500

    Add config for google analytics id and add to sphinx config for read the docs theme

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -155,7 +155,9 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {}
+if conf.GOOGLE_ANALYTICS_TRACKING_ID:
+    html_theme_options['analytics_id'] = conf.GOOGLE_ANALYTICS_TRACKING_ID
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
