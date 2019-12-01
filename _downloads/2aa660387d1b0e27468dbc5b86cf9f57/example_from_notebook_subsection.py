b131ad7e25aec79d680287a2ca527cfc7a180b3c
whoopnip <whoopnip@users.noreply.github.com>
2019-12-01 22:49:39 +0000

    Automated deployment: Sun Dec  1 22:49:39 UTC 2019 8651db6141d92e58754778eebe1ca718cee3186b

---
+++ _downloads/2aa660387d1b0e27468dbc5b86cf9f57/example_from_notebook_subsection.py
@@ -0,0 +1,26 @@
+"""
+Example Notebook in Subsection
+==============================
+
+I actually generated this notebook instead of taking it off the shelf.
+
+"""
+
+a = [1, 2, 3]
+for i in a:
+    print(i)
+
+
+######################################################################
+# And some Math
+# -------------
+# 
+
+
+######################################################################
+# Offset:
+# 
+# .. math:: \frac{1}{N}\sum_{i=i}{N}x_i
+# 
+# And then in :math:`\frac{1}{N}\sum_{i=i}{N}x_i` a paragraph.
+# 
