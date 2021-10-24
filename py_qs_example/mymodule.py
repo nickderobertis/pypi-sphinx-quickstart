528bd6632b758164bc9151d930a6eb9d16025622
Nick DeRobertis <whoopnip@gmail.com>
2021-10-24 07:30:29 -0400

    Remove example TODO from old TODO bot

--- py_qs_example/mymodule.py
+++ py_qs_example/mymodule.py
@@ -45,15 +45,6 @@ class ExampleClass:
     def __repr__(self):
         return 'example'
 
-# TODO [#6]: An example issue raised by todo-actions
-#
-# This message should automatically get converted into an issue. When this
-# message is removed, it should remove the issue.
-# - bullets
-# - are
-# - supported
-#
-# As well as multiple paragraphs.
 def example_function(thing: ExampleClass, arg: str) -> str:
     """
     Example for function documentation.
